bce6e38803a447d81518e97a9178529bbf0c0986
roshith <roshithprakash7@gmail.com>
2024-12-28 18:52:49 +0530

    Initial commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <>
+      <h1 className="p-2 font-semibold italic text-4xl">About</h1>
+      <Link href="/">Home</Link>
+    </>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <h1 className="p-2 font-semibold italic text-4xl">Gridbox Manager</h1>
+      <Link href="/about">About</Link>
     </>
   );
 }
